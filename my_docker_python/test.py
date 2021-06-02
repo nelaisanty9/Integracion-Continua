788649daaeaa0e0f908b0b14ae7de73b7162e26a
ulichovick@manjaro <ulichovick@gmail.com>
2021-06-01 20:03:51 -0500

    se agrega script de prueba de la conexión entre los contenedores

--- my_docker_python/test.py
+++ my_docker_python/test.py
@@ -1,1 +1,18 @@
-print("prueba del contenedor de python") 
+import mariadb
+import sys
+def conectar():
+    try:
+        conn = mariadb.connect(user = "root", database ="testDB", host = "mariadb", password = $
+    except mariadb.error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+    return conn
+
+con = conectar()
+cur = con.cursor()
+cons = 'select * from test'
+cur.execute(cons)
+que = cur.fetchall()
+for id,name in que:
+        print("id: " + str(id))
+        print("nombre: " + str(name))
